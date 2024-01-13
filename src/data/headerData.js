35acfa304d130b691ed39453960fdeac5cc950b7
krixfega <krixfega@gmail.com>
2024-01-13 17:44:16 +0100

    header adjusted

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import profileImage from '../assets/images/wumi.jpeg';
 export const headerData = {
     name: 'Esther O. Oladele',
     title: "Product Manager",
-    desciption:"I'm a passionate Product Manager with a knack for turning ideas into reality. Welcome to my digital playground! 🚀 Explore the intersection of my experiences, projects, and aspirations. Join me on a journey where creativity meets strategy, and innovation knows no bounds. Ready to dive into my world? Let's make things happen together! 🌟",
+    desciption:"I'm a passionate Product Manager with a knack for turning ideas into reality. 🚀 Explore the intersection of my experiences, projects, and aspirations. My journey is one where creativity meets strategy, and innovation knows no bounds. Ready to dive into my world? Let's make things happen together! 🌟",
     image: 'profileImage',
     resumePdf: resume
 }
